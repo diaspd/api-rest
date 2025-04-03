01fe6f459cfac922e11c2733af320c87fca597cc
Pedro Dias <pdr050203@gmail.com>
2025-04-02 22:38:55 -0300

    refactor: change transaction route url

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,7 +2,7 @@ import type { FastifyInstance } from "fastify";
 import { knex } from "../database";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
+  app.post("/transact", async () => {
     const transactions = await knex("transactions")
       .where("amount", 1000)
       .select("*");
